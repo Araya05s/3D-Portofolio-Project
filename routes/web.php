d3ee7bd62ff677ac3e4ce46b34cf1916f3b28ce4
Araya05s <akhsanaraya5@gmail.com>
2026-01-25 23:08:58 +0700

    Initialize Vercel for Web Publication

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use Laravel\Fortify\Features;
 
 Route::get('/', function () {
-    return Inertia::render('homepage', [
-        'canRegister' => Features::enabled(Features::registration()),
-    ]);
+    return Inertia::render('homepage', []);
 })->name('home');
 
-// Route::middleware(['auth', 'verified'])->group(function () {
-//     Route::get('dashboard', function () {
-//         return Inertia::render('dashboard');
-//     })->name('dashboard');
-// });
-
-Route::get('dashboard', function () {
-    return Inertia::render('dashboard');
-})->name('dashboard');
-
-//require __DIR__.'/settings.php';
