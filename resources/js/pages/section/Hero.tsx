abe29f3c68baf72b68161b4601a7757e96aa1e3a
Araya05s <akhsanaraya5@gmail.com>
2026-02-04 14:00:51 +0700

    Fixed hero section layout

--- resources/js/pages/section/Hero.tsx
+++ resources/js/pages/section/Hero.tsx
@@ -12,20 +12,24 @@ import React_Logo from "@/components/React_Logo";
 import Diamonds from "@/components/Diamonds";
 import { Fade, Slide } from "react-awesome-reveal";
 
-import site from "@/data/profile.json"
+import site_p from "@/data/profile.json"
+import site_c from "@/data/contacts.json"
 
 import { Typewriter } from 'react-simple-typewriter'
 
 const Hero = () => {
 
   const isMobile = useMediaQuery({maxWidth: 768});  
-  const { headline, subheadline } = site.home
-  const { title, paragraphs } = site.about
+  const { headline, subheadline } = site_p.home
+  const { title, paragraphs } = site_p.about
+  const resume_link = site_c['resume-link']
+  const connections = site_c.contacts
+
 
 
   return (
     <section id="home" className='min-h-screen flex flex-col relative'>
-        <div className="absolute inset-0 pt-20 sm:mt36 mt-32 px-5 sm:px-24 gap-3 bg-linear-to-b from-[#0E1117]/0 via-[#0E1117]/80 to-[#2A1E3F]">
+        <div className="absolute inset-0 pt-20 sm:mt32 mt-12 px-5 sm:px-24 gap-3 bg-linear-to-b from-[#0E1117]/0 via-[#0E1117]/80 to-[#2A1E3F]">
           <div className="group relative gap-4 z-10 flex flex-row">
             <div className="relative group w-18 h-18 sm:w-24 sm:h-24 flex items-center justify-center">
               <Fade duration={1000}>
@@ -35,7 +39,6 @@ const Hero = () => {
                   </div>
                 </Fade>
               </div>
-              <div>
               <div className='overflow-hidden'>
               <Fade direction='up' duration={1000} delay={250}>
                 <h1 className="sm:text-5xl text-xl text-white font-medium text-left font-generalsans">{headline} <span className="waving-hand">👋</span></h1>
@@ -52,11 +55,39 @@ const Hero = () => {
                 </p>
               </Fade>
             </div>
+            <div className="hidden lg:flex flex-wrap justify-left lg:gap-6 gap-0 lg:max-w-64 pt-2 ml-12">
+              {connections.map((contact) => (
+                <div
+                  key={contact.name}
+                >
+                  <Fade direction='up' duration={750} cascade damping={1000} delay={200}>
+                  <a
+                    href={contact.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="group relative flex items-center px-2 py-2 rounded-2xl bg-white/2 border border-white/10 backdrop-blur-md transition-all duration-500 ease-[cubic-bezier(0.23,1,0.32,1)] hover:-translate-y-3 hover:bg-white/[0.07] hover:border-purple-500/50 hover:shadow-[0_20px_40px_rgba(0,0,0,0.4),0_0_20px_rgba(168,85,247,0.15)] overflow-hidden"
+                  >
+                    <div className="absolute inset-0 -translate-x-full group-hover:translate-x-full transition-transform duration-1000 ease-in-out bg-linear-to-r from-transparent via-white/10 to-transparent skew-x-20" />
+    
+                    <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-500 bg-[radial-gradient(circle_at_center,var(--tw-gradient-stops))] from-purple-500/10 via-transparent to-transparent" />
+    
+                    <div className="relative z-10 p-1 rounded-lg bg-white/85 border border-white/5 group-hover:scale-110 group-hover:bg-white/85 group-hover:border-purple-400/30 transition-all duration-300">
+                      <img 
+                        src={contact.icon} 
+                        alt={contact.name} 
+                        className="w-7 h-7 object-contain brightness-90 group-hover:brightness-110" 
+                      />
+                    </div>
+                    <div className="absolute bottom-0 left-0 h-0.5 w-0 bg-linear-to-r from-transparent via-purple-400 to-transparent group-hover:w-full transition-all duration-700" />
+                    </a>
+                  </Fade>
+                </div>
+              ))}
             </div>
           </div>
           <div>
             <Fade duration={500}>
-              <h1 className="lg:mt-12 mt-6 sm:text-3xl text-xl text-white font-bold lg:text-left text-center underline decoration-purple-500 decoration-4 underline-offset-8 font-generalsans">{title}</h1>
+              <h1 className="lg:mt-6 mt-6 sm:text-3xl text-xl text-white font-bold lg:text-left text-center underline decoration-purple-500 decoration-4 underline-offset-8 font-generalsans">{title}</h1>
             </Fade>
             <Slide direction='left' duration={750} delay={150}>
               <p className='lg:max-w-180 w-full lg:pt-6 pt-4 text-justify relative z-10 font-light text-md md:text-lg text-slate-200 font-generalsans'>
@@ -64,7 +95,7 @@ const Hero = () => {
               </p>
             </Slide>
           </div>
-          <div className="hidden lg:block w-4xl h-full absolute right-0 top-0">
+          <div className="hidden lg:block w-3xl h-full absolute right-0 top-0">
             <Canvas className="w-full h-full">
               <Suspense fallback={<CanvasLoader />}>
                 <PerspectiveCamera makeDefault position={[-10, 2, 22]} />
@@ -83,17 +114,28 @@ const Hero = () => {
               </Suspense>
             </Canvas>
           </div>
-
-          <div className="visible lg:hidden w-full h-full absolute inset-0">
+          
+            <div className="visible lg:hidden w-full h-full absolute inset-0">
             <Canvas className="w-full h-full">
               <Suspense fallback={<CanvasLoader />}>
                 <PerspectiveCamera makeDefault position={[0, 0, 28]} />
                 <ambientLight intensity={1.0} />
                 <directionalLight position={[10, 10, 5]} intensity={2} />
-                <IntroModel position={[0, -4, 5]} rotation={[15, 60, 0]} scale={isMobile? 5.0 : 6.5}/>
+                <IntroModel position={[0, -6, 5]} rotation={[15, 60, 0]} scale={isMobile? 5.0 : 6.5}/>
               </Suspense>
             </Canvas>
           </div>
+          <div className="flex align-center justify-left text-left flex-row gap-8 pt-4">
+              <a
+                href= {resume_link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="relative lg:mt-4 mt-0 mb-24 w-fit px-5 py-2 text-center rounded-lg text-sm font-generalsans text-white overflow-hidden group inline-flex items-center justify-center font-bold transition-all duration-300 ease-out bg-linear-to-br from-purple-600 to-indigo-600 hover:scale-110 hover:rotate-1 active:scale-95 hover:shadow-[0_20px_50px_rgba(147,51,234,0.3)]"
+              >
+                <span className="absolute inset-0 align-middle justify-center bg-gradient-to from-purple-500/0 via-purple-500/40 to-purple-500/0 translate-x-full group-hover:translate-x-full transition-transform duration-700" />
+                <span className="relative z-10">Check out My resume!</span>
+              </a>
+            </div>
         </div>
 
     </section>
