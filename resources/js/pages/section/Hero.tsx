a3b355146b892135a5d5ff9056b2b4da4a0d7d53
Araya05s <akhsanaraya5@gmail.com>
2026-08-06 21:58:40 +0700

    feat: Updated Responsiveness in the Hero sections

--- resources/js/pages/section/Hero.tsx
+++ resources/js/pages/section/Hero.tsx
@@ -18,8 +18,8 @@ import site_p from "@/data/profile.json"
 
 const Hero = () => {
 
-  const isMobileWidth = useMediaQuery({maxWidth: 768});  
-  const isMobileHeight = useMediaQuery({maxHeight: 768});  
+  const isMobileWidth = useMediaQuery({maxWidth: 768});
+  const isMobileHeight = useMediaQuery({maxHeight: 768});
   const { headline, subheadline } = site_p.home
   const { title, paragraphs } = site_p.about
   const resume_link = site_c['resume-link']
@@ -29,7 +29,7 @@ const Hero = () => {
 
   return (
     <section id="home" className='min-h-screen flex flex-col relative'>
-        <div className="absolute inset-0 pt-20 sm:mt32 mt-12 px-5 sm:px-24 gap-3 bg-linear-to-b from-[#0E1117]/0 via-[#0E1117]/80 to-[#2A1E3F]">
+        <div className="pt-20 pb-128 md:pb-24 sm:mt32 mt-12 px-5 sm:px-24 gap-3 bg-linear-to-b from-[#0E1117]/0 via-[#0E1117]/80 to-[#2A1E3F]">
           <div className="relative gap-4 z-10 flex flex-row">
             <div className="relative group w-18 h-18 sm:w-24 sm:h-24 flex items-center justify-center">
               <Fade duration={1000}>
@@ -45,7 +45,7 @@ const Hero = () => {
                 <p className="sm:text-2xl text2xl text-neutral-300 font-extrabold text-left font-generalsans">
                 <Typewriter
                   words={subheadline}
-                  loop={0}              
+                  loop={0}
                   cursor
                   cursorStyle="|"
                   typeSpeed={75}
@@ -65,17 +65,17 @@ const Hero = () => {
                     href={contact.link}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="group relative flex items-center px-2 py-2 rounded-2xl bg-white/2 border border-white/10 backdrop-blur-md transition-all duration-500 ease-[cubic-bezier(0.23,1,0.32,1)] hover:-translate-y-3 hover:bg-white/[0.07] hover:border-purple-500/50 hover:shadow-[0_20px_40px_rgba(0,0,0,0.4),0_0_20px_rgba(168,85,247,0.15)] overflow-hidden"
+                    className="group relative flex items-center px-2 py-2 rounded-2xl bg-white/2 border border-white/10 backdrop-blur-md transition-all duration-500 ease-[cubic-bezier(0.23,1,0.32,1) hover:scale-105 hover:bg-white/[0.07] hover:border-purple-500/50 hover:shadow-[0_20px_40px_rgba(0,0,0,0.4),0_0_20px_rgba(168,85,247,0.15)] overflow-hidden"
                   >
                     <div className="absolute inset-0 -translate-x-full group-hover:translate-x-full transition-transform duration-1000 ease-in-out bg-linear-to-r from-transparent via-white/10 to-transparent skew-x-20" />
-    
+
                     <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-500 bg-[radial-gradient(circle_at_center,var(--tw-gradient-stops))] from-purple-500/10 via-transparent to-transparent" />
-    
+
                     <div className="relative z-10 p-1 rounded-lg bg-white/85 border border-white/5 group-hover:scale-110 group-hover:bg-white/85 group-hover:border-purple-400/30 transition-all duration-300">
-                      <img 
-                        src={contact.icon} 
-                        alt={contact.name} 
-                        className="w-7 h-7 object-contain brightness-90 group-hover:brightness-110" 
+                      <img
+                        src={contact.icon}
+                        alt={contact.name}
+                        className="w-7 h-7 object-contain brightness-90 group-hover:brightness-110"
                       />
                     </div>
                     <div className="absolute bottom-0 left-0 h-0.5 w-0 bg-linear-to-r from-transparent via-purple-400 to-transparent group-hover:w-full transition-all duration-700" />
@@ -91,7 +91,7 @@ const Hero = () => {
             </Fade>
             <Slide direction='left' duration={750} delay={150}>
               <p className='lg:max-w-180 w-full lg:pt-6 pt-4 text-justify relative z-10 font-light text-sm md:text-lg sm:text-md text-slate-200 font-generalsans'>
-                {paragraphs}            
+                {paragraphs}
               </p>
             </Slide>
           </div>
@@ -104,7 +104,7 @@ const Hero = () => {
                 <directionalLight position={[10, 10, 5]} intensity={0.5} />
                 <IntroModel position={[0, 0, 0]} rotation={[15, 60, 0]} scale={8}/>
 
-                <group> 
+                <group>
                   <directionalLight position={[-10, 10, 5]} intensity={1.75} />
                   <Donut scale={25} position={[-7.4, -5, 3]} rotation={[-5.9, -6, 0]}/>
                   <Int_Cube scale={3} position={[6.7, 3, 6]} rotation={[-5.9, -4, 0]}/>
@@ -114,7 +114,7 @@ const Hero = () => {
               </Suspense>
             </Canvas>
           </div>
-          
+
             <div className="visible lg:hidden w-full h-full  absolute inset-0 mb-8">
             <Canvas className="w-full h-full">
               <Suspense fallback={<CanvasLoader />}>
@@ -131,7 +131,7 @@ const Hero = () => {
                 href= {resume_link}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="relative lg:mt-4 mt-0 mb-24 w-fit px-5 py-2 text-center rounded-lg text-sm font-generalsans text-white overflow-hidden group inline-flex items-center justify-center font-bold transition-all duration-300 ease-out bg-linear-to-br from-purple-600 to-indigo-600 hover:scale-110 hover:rotate-1 active:scale-95 hover:shadow-[0_20px_50px_rgba(147,51,234,0.3)]"
+                className="relative lg:mt-4 mt-0 mb-28 lg:mb-24 w-fit px-5 py-2 text-center rounded-lg text-sm font-generalsans text-white overflow-hidden group inline-flex items-center justify-center font-bold transition-all duration-300 ease-out bg-linear-to-br from-purple-600 to-indigo-600 hover:scale-110 hover:rotate-1 active:scale-95 hover:shadow-[0_20px_50px_rgba(147,51,234,0.3)]"
               >
                 <span className="absolute inset-0 align-middle justify-center bg-gradient-to from-purple-500/0 via-purple-500/40 to-purple-500/0 translate-x-full group-hover:translate-x-full transition-transform duration-700" />
                 <span className="relative z-10">Check out My resume!</span>
